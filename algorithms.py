1ef813efa40ca27433896a71f709486a01fd1923
JGMorgan <josegm11194@gmail.com>
2016-10-30 23:29:17 -0400

    added problem

--- algorithms.py
+++ algorithms.py
@@ -101,6 +101,47 @@ def swap(li, x, y):
 	return li
 
 
+'''Find shortest unique prefix to represent each word in the list.
+This question didn't make much sense to me when
+I first read it but basically you are supposed
+return the shortest prefix that doesn't exist in any other word
+if given ['dog', 'duck'] both begin with 'd' so the shortest prefixes
+are ['do', 'du']'''
+class Trie:
+    def __init__(self):
+        self.table = {}
+        self.val = 0
+
+def prefix(A):
+        out = []
+        trie = Trie()
+        for x in A:
+            temp = trie
+            for c in x:
+                if c in temp.table:
+                    temp.table[c].val += 1
+                    print c
+                    print temp.table[c].val
+                    temp = temp.table[c]
+                else:
+                    temp.table[c] = Trie()
+                    temp.table[c].val = 1
+                    print c
+                    print temp.table[c].val
+                    temp = temp.table[c]
+        for i in xrange(len(A)):
+            temp = trie
+            out.append('')
+            for c in A[i]:
+                if temp.table[c].val == 1:
+                    out[i]+=c
+                    break
+                else:
+                    temp = temp.table[c]
+                    out[i]+=c
+        return out
+
+
 if __name__ == '__main__':
 	print count_trailing_zeros_in_fact(25)
 	print powerset([1,2,3])
@@ -112,3 +153,4 @@ if __name__ == '__main__':
     print count_1_bits(10)
     print how_many_flips(10,5)
     print swap([1,2,3],0,2)
+    print prefix([ "zebra", "dog", "duck", "dot" ])
