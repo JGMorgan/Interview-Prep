10677e0ec4b8443d6f394f0ff7d9fa881e1f4f24
JGMorgan <josegm11194@gmail.com>
2016-11-01 13:49:37 -0400

    new code alert

--- algorithms.py
+++ algorithms.py
@@ -230,6 +230,67 @@ def isPowerOfTwo(n):
     return a & (a-1) == 0
 
 
+'''
+O(sqrt(n)) time complexity
+O(1) space complexity
+Checks if a number is prime, fun fact 1 is not prime
+'''
+import math
+def isPrime(n):
+    if n == 1 or n == 2:
+        return False
+    else:
+        for i in xrange(2, int(math.sqrt(n))):
+            if n % i == 0:
+                return False
+        return True
+
+
+'''
+O(n) time complexity
+O(n) space complexity
+Given an infinite amount of pennies, dimes,
+nickels and quarters how many ways can we make
+n cents
+'''
+def ways_to_make_change(n):
+    coins = [1,5,10,25]
+    table = [[0 for _ in xrange(len(coins))] for _ in xrange(n+1)]
+    #how many ways can we make 0 cents with our coins
+    #there is only 1 way to make any amount with only pennies
+    for i in xrange(len(coins)):
+        table[0][i] = 1
+    for i in xrange(n+1):
+        table[i][0] = 1
+    for i in xrange(1, n+1):
+        for j in xrange(1, len(coins)):
+            x = 0
+            if i - coins[j] >= 0:
+                x = table[i - coins[j]][j]
+            y = table[i][j-1]
+            table[i][j] = x + y
+    return table[n][len(coins)-1]
+
+
+'''
+O(m*n) time complexity
+O(m*n) space complexity
+Given an m x n matrix, how many ways can you get from
+the top left to the bottom left of the matrix assuming
+you can move right, down and bottom right
+'''
+def ways_from_topleft_to_bottomleft(m, n):
+    table = [[0 for _ in xrange(m)] for _ in xrange(n)]
+    for i in xrange(m):
+        table[0][i] = 1
+    for i in xrange(n):
+        table[i][0] = 1
+    for i in xrange(1, n):
+        for j in xrange(1, m):
+            table[i][j] = table[i-1][j-1] + table[i][j-1] + table[i-1][j]
+    return table[n-1][m-1]
+
+
 if __name__ == '__main__':
     print count_trailing_zeros_in_fact(25)
     print powerset([1,2,3])
@@ -245,3 +306,6 @@ if __name__ == '__main__':
     print next_palindome(19)
     print minesweeper(5,5,10)
     print isPowerOfTwo(128)
+    print isPrime(73)
+    print ways_to_make_change(420)
+    print ways_from_topleft_to_bottomleft(6, 5)
